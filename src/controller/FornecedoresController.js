a0660182bb331e4406396d6abd134edbef1986fb
larrissalira <larrissalira@outlook.com>
2022-08-02 11:43:50 -0300

    cria rq PUT e DELETE para listarId e fazer updateId na rota /controller/FornecedoresController.js

--- src/controller/FornecedoresController.js
+++ src/controller/FornecedoresController.js
@@ -33,8 +33,37 @@ class FornecedoresController {
               res.status(400).json(error.message)
           }
           })
-        }
+        app.put("/fornecedores/:id", async (req, res)=> {
+          const validar = FornecedoresValidacao.validar(...Object.values(req.body))
+          try{
+            const fornecedores = await FornecedoresMetodos.fornecedoresListar(req.params.id_fornecedores)
+            if(!fornecedores){
+              throw new Error("Id inexistente, tente novamente.")
+            }
+            if(validar){
+              const fornecedores = new FornecedoresModel(...Object.values(req.body))
+              const response = FornecedoresMetodos.fornecedoresUpdate(req.params.id_fornecedores, fornecedores)
+              res.status(201).json(response)
+            }else{
+                throw new Error("Erro, tente novamente.")
+            }
+          }catch(error){
+            res.status(400).json(error.message)
+          }
+        })
+        app.delete("/fornecedores/:id", async (req, res) => {
+          try {                
+              const fornecedores = await FornecedoresMetodos.fornecedoresDeletar(req.params.id)
+              if(!fornecedores){
+                  throw new Error("Erro, tente novamente.")
+              }
+              res.status(200).json(fornecedores)
+          }catch (error){    
+            console.log(error)
+              res.status(404).json({Error: error.message})
+          }            
+        })
       }
-      
+    }
 
-export default FornecedoresController
+export default FornecedoresController;

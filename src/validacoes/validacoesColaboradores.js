c25d21957bb6958f6b7f9fbd740ab9b7b495c5d2
devhelderlrs <helderl4004@gmail.com>
2022-08-03 13:56:00 -0300

    Desenvolvimento validações especfícas para entidade colaboradores

--- src/validacoes/validacoesColaboradores.js
+++ src/validacoes/validacoesColaboradores.js
@@ -1,6 +1,6 @@
-const nome = 'lucas'
+import ValidacoesGerais from './ValidacoesGerais.js';
 
-class ValidacoesGerais{    
+class ValidacoesColaboradores extends ValidacoesGerais{    
     static validaNome(nome){
         if(!nome.length >= 3){
             console.log('error 3 digitos');
@@ -8,6 +8,24 @@ class ValidacoesGerais{
             console.log('error tipo string')
         }
     }
+
+    static validaCpf(cpf){
+        return cpf.length === 14 ? true : false;
+    }
+
+    static validaCargo(cargo){
+        return cargo.length >= 6 ? true : false;
+    }
+
+    static validaSalario(salario){
+        const lista = '0123456789.,R$ ';
+
+        return salario.split('').every(salario => lista.includes(salario))
+    }
 }
 
-ValidacoesGerais.validaNome(nome)
+// const cpf = '014.578.963-40'
+
+// console.log(cpf.length)
+
+console.log(ValidacoesColaboradores.validaSalario('R$ l1643,47'))

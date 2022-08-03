f51529f2b86f2693e91bc6328a80f8f1472816b4
larrissalira <larrissalira@outlook.com>
2022-08-02 23:17:35 -0300

    remoção do 'id' no constructor na /model/FornecedoresModel.js

--- src/model/FornecedoresModel.js
+++ src/model/FornecedoresModel.js
@@ -1,6 +1,5 @@
 class FornecedoresModel {
-    constructor({ id_fornecedor, nome_fornecedor, cnpj_fornecedor, endereco_fornecedor, ramo_fornecedor, email_fornecedor, telefone_fornecedor}){
-        this.id_fornecedor = id_fornecedor;
+    constructor(nome_fornecedor, cnpj_fornecedor, endereco_fornecedor, ramo_fornecedor, email_fornecedor, telefone_fornecedor){
         this.nome_fornecedor = nome_fornecedor;
         this.cnpj_fornecedor = cnpj_fornecedor;
         this.endereco_fornecedor = endereco_fornecedor;
